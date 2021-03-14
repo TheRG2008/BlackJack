4482e53b0c86df7ce27783a926bf6ae507df8706
TheRG <TheRG2008@gmail.com>
2021-03-14 21:13:22 +0500

    Add

---
+++ BlackJack.cs
@@ -0,0 +1,187 @@
+﻿using System;
+
+namespace Black_Jack
+{
+    class BlackJack
+    {
+        static void Main(string[] args)
+        {
+
+            int gold = 300;
+            int rate = 0;            
+            int card = 0;
+            int card1 = 0;
+            int card2 = 0;
+            int score = 0;
+            int dillerCard = 0;
+            int dillerCard1 = 0;
+            int dillerCard2 = 0;
+            int dillerScore = 0;
+           
+
+            Console.WriteLine($"Добро пожаловать в наше казино! Есть одно свободное место за столом Black Jack!");
+            
+            Game:
+
+            while (gold != 0)
+            {
+                Console.Clear();
+                BJ.NewGame();
+                rate = BJ.GetRate(gold);
+                gold = BJ.GetGold(rate, gold);
+                Console.WriteLine("Диллер сдает карты");
+                Console.WriteLine("Вам раздали 2 карты");
+
+                card1 =  BJ.GetCard();                
+                card2 = BJ.GetCard();                
+                score = card1 + card2;
+                Console.WriteLine($"Всего очков: {score}");
+                Console.ReadKey();
+
+               
+                if (score == 21)
+                {
+                    gold = BJ.PlayerWin(gold, rate);                 
+                    goto Game;
+                }
+               
+                while (score <= 21)
+                {
+                    Console.WriteLine("ЕЩЕ? - Y/N");
+                    char next = Console.ReadKey(true).KeyChar;
+                    if (next == 'y')
+                    {
+                        card = BJ.GetCard();                       
+                        score += card;
+
+                        Console.WriteLine($"Всего очков {score}");
+                        Console.ReadKey();
+
+                        if (score > 21)
+                        {
+                            if (card1 == 11 || card2 == 11 || card == 11)
+                            {
+                                score = BJ.CheckAce(ref card1, ref card2, ref card);
+                                Console.WriteLine($"Всего очков {score}");
+                                Console.ReadKey();
+                                continue;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Вы проиграли!");
+                                Console.ReadKey();
+                                goto Game;
+                            }
+     
+                        }
+                        else if (score == 21)
+                        {
+                            gold = BJ.PlayerWin(gold, rate);
+                            goto Game;
+                        }
+                        else continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Играет диллер");
+                        dillerCard1 = BJ.GetCard();                        
+                        dillerCard2 = BJ.GetCard();
+                        dillerScore = dillerCard1 + dillerCard2;
+
+                        DillerGame:
+
+                        if (dillerScore > 21)
+                        {
+                            if (dillerCard1 == 11 || dillerCard2 == 11 || dillerCard == 11)
+                            {
+                                dillerScore = BJ.CheckAce(ref dillerCard1, ref dillerCard2, ref dillerCard);
+                                Console.WriteLine($"Диллер набрал {dillerScore} очков");
+                                Console.ReadKey();
+                                goto DillerGame;
+                            }
+                            else
+                            {
+                                Console.WriteLine("У Диллера - перебор");
+                                gold = BJ.PlayerWin(gold, rate);
+                                goto Game;
+                            }                               
+    
+                        }
+                        else if (dillerScore >= 16 && dillerScore <=21)
+                        {
+                            Console.WriteLine($"Диллер набрал {dillerScore} очков");
+                            if (dillerScore > score)
+                            {
+                                Console.WriteLine("Вы проиграли!");
+                                goto Game;
+                            }
+                            else if (dillerScore < score)
+                            {
+                                Console.WriteLine($"Диллер набрал {dillerScore} очков");
+                                gold = BJ.PlayerWin(gold, rate);
+                                goto Game;
+                            }
+                            else if (dillerScore == score)
+                            {
+                                Console.WriteLine($"Диллер набрал {dillerScore} очков");
+                                Console.WriteLine("Ничья");
+                                Console.ReadKey();
+                                gold += rate;
+                                goto Game;
+                            }
+                        }
+                        else if (dillerScore < 16)
+                        {
+                            Console.WriteLine($"Диллер берет еще карту");
+                            dillerCard = BJ.GetCard();
+                            dillerScore += dillerCard;
+                            Console.WriteLine($"Диллер набрал {dillerScore} очков");
+                            goto DillerGame;
+                        }
+                        Console.ReadKey();
+
+                    }
+
+                }
+            }
+
+            Console.WriteLine("Вы проиграли все деньги, покиньте наше казино");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("псс.... что все денежки тю тю.. могу помочь");
+            Console.ReadKey();
+            Console.WriteLine("Могу купить у тебя что нибудь!");
+            Console.ReadKey();
+
+            Console.WriteLine("Вещи на продажу:\n 1.Часы - 100$\n 2.Пиджак - 50$\n 3.Цепочка - 80$\n 4.Трусы - 20$");
+            int sale = int.Parse(Console.ReadLine());
+
+            switch (sale)
+            {
+                case 1: gold += 100; break;
+                case 2: gold += 50; break;
+                case 3: gold += 80; break;
+                case 4: gold += 20; break;
+                        
+            }
+
+            Console.WriteLine($"Хотите вернуться в казино? - Y/N");
+            char newGame = Console.ReadKey(true).KeyChar;
+            if (newGame != 'y')
+            {
+                Console.WriteLine("До свиданья!");
+                BJ.EndGame();
+            }
+            else
+            {
+                Console.WriteLine($"И снова здравствуйте! Ваше место еще свободно - проходите! ");
+                goto Game;
+            }
+
+
+        }
+    }
+}
+
+
---
+++ Class1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Black_Jack
+{
+    class BJ
+    {
+        /// <summary>
+        /// Рандомное число для генерации карты
+        /// </summary>
+        /// <returns>Возвращает случайное число от 2 до 14 </returns>
+        public static int RandomNumber()
+        {
+            var r1 = new Random();
+            int random = r1.Next(1, 14);
+            return random;
+        }
+        /// <summary>
+        /// Генерирует название карты
+        /// </summary>
+        /// <param name="random"></param>
+        /// <returns>Возвращает название карты</returns>
+        public static string GetCardName(int random)
+        {
+            string cardName = " ";
+            switch (random)
+            {
+                case 1: cardName = "ДВОЙКА"; break;
+                case 2: cardName = "ТРОЙКА"; break;
+                case 3: cardName = "ЧЕТВЕРКА"; break;
+                case 4: cardName = "ПЯТЕРКА"; break;
+                case 5: cardName = "ШЕСТЕРКА"; break;
+                case 6: cardName = "СЕМЕРКА"; break;
+                case 7: cardName = "ВОСЬМЕРКА"; break;
+                case 8: cardName = "ДЕВЯТКА"; break;
+                case 9: cardName = "ДЕСЯТКА"; break;
+                case 10: cardName = "ВАЛЕТ"; break;
+                case 11: cardName = "ДАМА"; break;
+                case 12: cardName = "КОРОЛЬ"; break;
+                case 13: cardName = "ТУЗ"; break;
+            }
+            return cardName;
+        }
+        /// <summary>
+        /// Присваивает значение для сгенерированной карты
+        /// </summary>
+        /// <param name="random"></param>
+        /// <returns>Возвращает значение карты</returns>
+        public static int GetCardValue(int random)
+        {
+            int cardValue = 0;
+            switch (random)
+            {
+                case 1: cardValue = 2; break;
+                case 2: cardValue = 3; break;
+                case 3: cardValue = 4; break;
+                case 4: cardValue = 5; break;
+                case 5: cardValue = 6; break;
+                case 6: cardValue = 7; break;
+                case 7: cardValue = 8; break;
+                case 8: cardValue = 9; break;
+                case 9: cardValue = 10; break;
+                case 10: cardValue = 10; break;
+                case 11: cardValue = 10; break;
+                case 12: cardValue = 10; break;
+                case 13: cardValue = 11; break;
+            }
+            return cardValue;
+        }
+        /// <summary>
+        /// Продолжаем играть или нет
+        /// </summary>
+        public static void NewGame()
+        {
+
+            Console.WriteLine($"Желаете сыграть? - Y/N");
+            char newGame = Console.ReadKey(true).KeyChar;
+            if (newGame != 'y')
+            {
+                Console.WriteLine("До свиданья!");
+                EndGame();
+
+            } 
+
+        }
+        /// <summary>
+        /// Определяет ставку Игрока
+        /// </summary>
+        /// <param name="gold"></param>
+        /// <returns></returns>
+        public static int GetRate(int gold)
+        {
+            int rate;
+            Rate:
+            do
+            {
+                Console.WriteLine($"У ваc {gold}$ Делайте вашу ставку:");
+            } while (!int.TryParse(Console.ReadLine(), out rate));
+
+           
+            if (rate > gold)
+            {
+                Console.WriteLine("У вас недостаточно денег, сделайте другую ставку!");
+                goto Rate;
+            }
+            else
+            {
+                gold -= rate;
+                Console.WriteLine($"Ставка {rate} принята. У вас осталось - {gold}$ ");
+                Console.ReadLine();
+            }
+            return rate;
+            
+        }
+        /// <summary>
+        /// Определяет сколько осталось денег
+        /// </summary>
+        /// <param name="rate"></param>
+        /// <param name="gold"></param>
+        /// <returns></returns>
+        public static int GetGold(int rate, int gold)
+        {
+            gold -= rate;
+            return gold;
+        }
+        /// <summary>
+        /// Выход из игры
+        /// </summary>
+        public static void EndGame()
+        {
+            Environment.Exit(0);
+        }
+        /// <summary>
+        /// Генерирует случайную карту
+        /// </summary>
+        /// <returns>Возвращает значение этой карты</returns>
+        public static int GetCard()
+        {
+
+            int r = RandomNumber();
+            string cardName = GetCardName(r);
+            int cardValue = GetCardValue(r);
+
+            Console.WriteLine($"{cardName} - {cardValue} очков");
+            Console.ReadKey();
+
+            int value = cardValue;
+            return value;
+
+        }
+        /// <summary>
+        /// Проверяет наличие Туза, если перебор
+        /// </summary>
+        /// <param name="card1"></param>
+        /// <param name="card2"></param>
+        /// <param name="card"></param>
+        /// <returns>Возвращает итоговую сумму значений карт</returns>
+        public static int CheckAce(ref int card1,  ref int card2, ref int card)
+        {
+            int score = 0;
+            if (card1 == 11) 
+            {
+                card1 = 1;
+                return score  = card1 + card2 + card; 
+            } 
+            else if (card2 == 11)
+            {
+                card2 = 1;
+                return score = card2 + card1 + card;
+            }
+            else if (card == 11)
+            {
+                card = 1; 
+                return score = card + card1 + card2;
+            }
+            else return score = card + card1 + card2;
+        }
+        /// <summary>
+        /// Победа игрока
+        /// </summary>
+        /// <param name="gold"></param>
+        /// <param name="rate"></param>
+        /// <returns>Возвращает кол-во денег у игрока</returns>
+        public static int PlayerWin(int gold, int rate)
+        {
+            Console.WriteLine("Вы победили!");
+            Console.ReadKey();
+            gold += rate * 2;
+            return gold;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
